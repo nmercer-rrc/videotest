2f6a664725c1db4e8e31a5988a590f01f8425c09
nmercer-rrc <nmercer@academic.rrc.ca>
2025-05-28 10:42:40 -0500

    Edited socket to include security protocol to fix bad gateway error.

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ const io = new Server(server, {
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
 // Socket.IO connection
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
